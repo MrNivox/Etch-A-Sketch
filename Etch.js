a77e52e2aea47e62e78e0509bc9305ae25613ae3
MrNivox <85137683+MrNivox@users.noreply.github.com>
2024-12-11 19:29:11 -0500

    addedthe grid fucntion and a css style file

--- Etch.js
+++ Etch.js
@@ -1,7 +1,18 @@
 
+const containe = document.querySelector("div")
+
+function range(range)
+{
+ for(i = 0; i < range + 1; i++)
+ {
+  const div = document.createElement("div")
+  div.innerText = i
+  containe.appendChild(div)
+
+ }
 
 
+}
 
-
-
+range(16)
 
