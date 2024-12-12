918f98ddf4c89ed49fb3c31b2aa13f2c1fd712cf
MrNivox <85137683+MrNivox@users.noreply.github.com>
2024-12-11 22:56:32 -0500

    range function to make grid added and css flex

--- Etch.js
+++ Etch.js
@@ -3,10 +3,15 @@ const containe = document.querySelector("div")
 
 function range(range)
 {
- for(i = 0; i < range + 1; i++)
+ for(i = 0; i < (range * range ) ; i++)
  {
   const div = document.createElement("div")
   div.innerText = i
+  div.id = "item"
+  div.style.width = 750/range  + "px"
+  div.style.height = 750/range + "px"
+  
+  
   containe.appendChild(div)
 
  }
